6b6379cdbade2bea3621c055782259a939475ff9
Thomas Rebele <thomas.rebele@gmail.com>
2025-10-22 17:36:32 +0200

    move files

--- cleaning/import-metastore.sh
+++ cleaning/import-metastore.sh
@@ -33,22 +33,20 @@ PSQL_CMD=$(make_psql_cmd "-U hive metastore")
 
 # import dump
 DUMP_FILE="$(basename "$HOST_DUMP_FILE")"
-# TODO: reactivate!
 printf "\n\nsetup database\n"
-# podman cp "setup.sql" "$IMPORT_CONTAINER:/tmp/setup.sql"
-# exec_cmd "$IMPORT_CONTAINER" "cat /tmp/setup.sql | $(make_psql_cmd "postgres")"
-# 
-# printf "\n\nimport dump\n"
-# podman cp "$HOST_DUMP_FILE" "$IMPORT_CONTAINER:/tmp/$DUMP_FILE"
-# exec_cmd "$IMPORT_CONTAINER" "(printf '\\c metastore\n'; zstdcat '/tmp/$DUMP_FILE') | $PSQL_CMD"
-# 
-# 
-# podman cp "cleanup.sql" "$IMPORT_CONTAINER:/tmp/cleanup.sql"
-# exec_cmd "$IMPORT_CONTAINER" "(printf '\\c metastore\n'; cat /tmp/cleanup.sql) | $PSQL_CMD"
+podman cp "setup.sql" "$IMPORT_CONTAINER:/tmp/setup.sql"
+exec_cmd "$IMPORT_CONTAINER" "cat /tmp/setup.sql | $(make_psql_cmd "postgres")"
 
-# TODO
-# - remove temporary files
-# - create image:
-#   podman commit --include-volumes -q hive-postgres-metastore-import <image name, e.g., hive-postgres-metastore-tpc-ds-30tb-cleaned-up>
-#
-#
+printf "\n\nimport dump\n"
+podman cp "$HOST_DUMP_FILE" "$IMPORT_CONTAINER:/tmp/$DUMP_FILE"
+exec_cmd "$IMPORT_CONTAINER" "(printf '\\c metastore\n'; zstdcat '/tmp/$DUMP_FILE') | $PSQL_CMD"
+
+
+podman cp "cleanup.sql" "$IMPORT_CONTAINER:/tmp/cleanup.sql"
+exec_cmd "$IMPORT_CONTAINER" "(printf '\\c metastore\n'; cat /tmp/cleanup.sql) | $PSQL_CMD"
+
+exec_cmd "$IMPORT_CONTAINER" "rm /tmp/$DUMP_FILE; rm /tmp/setup.sql; rm /tmp/cleanup.sql"
+
+exec_cmd "$IMPORT_CONTAINER" 'su -l postgres -s /usr/bin/perl -- /usr/bin/pg_dump metastore' | zstd -10 > clean-dump.zstd
+
+
